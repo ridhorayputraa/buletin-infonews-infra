e2e8aea63e8569390dcf6119e9fb24bfbca83b0b
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-05 18:33:44 +0700

    fixing: create label for sign

--- database/migrations/2023_08_03_170813_create_authors_table.php
+++ database/migrations/2023_08_03_170813_create_authors_table.php
@@ -9,6 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    //  this table is for admin data
     public function up(): void
     {
         Schema::create('authors', function (Blueprint $table) {
