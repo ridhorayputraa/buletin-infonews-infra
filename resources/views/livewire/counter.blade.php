dd9a281142d95dd705f9f06d30025889ea28db06
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-12 13:16:13 +0700

    chore: adding livewire

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
 <div style="text-align: center">
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
-
+{{-- Count function --}}
     <h2>{{$countMinus}}</h2>
     <button wire:click="decrement">-</button>
 
