c65c088235d08e54236ba1b3256a46410f845d96
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-17 16:15:35 +0700

    chore: layouting flex in home content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,23 @@
 
 @section('container')
     {{-- <p>{{$categories->news[0]}}</p> --}}
-    <h1>{{ $news[0]->title }}</h1>
-    <h1>{{ $news[0]->category->name }}</h1>
-    <h1>{{ $news[0]->authors->name }}</h1>
-    <h1>{{ $news[0]->excerpt }}</h1>
-    <h1>{{ $news[0]->body }}</h1>
-    <img width="800px" height="800px" src="{{ asset('storage/images/' . $news[0]->thumbnail) }}" alt=""
-        title="" />
-    <p>{{ $news[0]->photo_description }}</p>
+    <div class="border border-red-500 flex w-full">
+
+        <div class="w-3/6">
+
+            <img width="656" height="399" src="{{ asset('storage/images/' . $news[0]->thumbnail) }}" alt=""
+                title="" />
+        </div>
+
+        <div class="w-3/6">
+
+            <h1>{{ $news[0]->title }}</h1>
+            <h1>{{ $news[0]->category->name }}</h1>
+            <h1>{{ $news[0]->authors->name }}</h1>
+            <h1>{{ $news[0]->excerpt }}</h1>
+            <h1>{{ $news[0]->body }}</h1>
+
+            <p>{{ $news[0]->photo_description }}</p>
+        </div>
+    </div>
 @endsection
