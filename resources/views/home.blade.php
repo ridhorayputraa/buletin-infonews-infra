381e7920a84f5972c237ae87661151a1947ee386
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-18 15:02:37 +0700

    chore: layouting adding text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,33 @@
 
 @section('container')
     {{-- <p>{{$categories->news[0]}}</p> --}}
-    <div class="border border-red-500 justify-between flex w-full">
+    <div class="border border-red-500 mt-10 justify-between flex w-full">
 
-        <div class="w-3/6 mr-5 border border-redHeavy">
+        <div class="w-3/6 mr-5 ">
 
-            <img width="656" height="399" src="{{ asset('storage/images/' . $news[0]->thumbnail) }}" alt=""
-                title="" />
+            <img class="rounded-3xl	" width="656" height="399" src="{{ asset('storage/images/' . $news[0]->thumbnail) }}"
+                alt="" title="" />
         </div>
 
-        <div class="w-3/6 mr-5 border border-redHeavy">
+        <div class="w-3/6 mr-5 border  border-redHeavy">
 
-            <h1>{{ $news[0]->title }}</h1>
-            <h1>{{ $news[0]->category->name }}</h1>
-            <h1>{{ $news[0]->authors->name }}</h1>
+            <div class="flex  items-center gap-2 ">
+                <a href="{{ route('category.show', $news[0]->category->slug) }}"
+                    class="font-poppins font-normal text-base text-redHeavy">{{ $news[0]->category->name }}</a>
+                <div>.</div>
+                <p class="font-poppins font-normal text-base">{{ $news[0]->authors->name }}</p>
+                <div>.</div>
+                <p class="font-poppins font-normal text-base">Published:
+                    {{ \Carbon\Carbon::createFromTimeStamp(strtotime($news[0]->published_at))->diffForHumans() }}
+                </p>
+            </div>
+
+            <p>{{ $news[0]->title }}</p>
+
+            <p>{{ $news[0]->photo_description }}</p>
+
             <h1>{{ $news[0]->excerpt }}</h1>
             <h1>{{ $news[0]->body }}</h1>
-            <p>Published: {{ \Carbon\Carbon::createFromTimeStamp(strtotime($news[0]->published_at))->diffForHumans() }}</p>
-            <p>{{ $news[0]->photo_description }}</p>
         </div>
     </div>
 @endsection
