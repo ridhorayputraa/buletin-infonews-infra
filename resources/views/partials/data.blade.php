f514de03fbfc3cd81d183bb9f34a1312b6fc4733
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-23 12:38:51 +0700

    feat: layouting for each data row

--- resources/views/partials/data.blade.php
+++ resources/views/partials/data.blade.php
@@ -3,14 +3,21 @@
 
 <div class="wrapper mt-20">
     <div class="heading-class flex  justify-between">
-        <p class="font-poppins text-blackPrimary font-semibold text-2xl">Terbaru</p>
-        <div class="flex w-52 rounded-2xl gap-2 py-2 justify-center radius items-center text-center bg-radius box">
+        <p class="font-poppins duration-300  hover:text-redHeavy  text-blackPrimary font-semibold text-2xl">Terbaru</p>
+        <div class="flex w-48 rounded-2xl gap-2 py-2 justify-center radius items-center text-center bg-radius box">
             <p class='text-redHeavy text-lg font-medium font-poppins'>Selengkapnya</p>
-            <img class="" width="16px" src="{{ asset('images/ArrowToRight.svg') }}" alt="Informa story">
+            <img class="" width="12px" src="{{ asset('images/ArrowToRight.svg') }}" alt="Informa story">
         </div>
     </div>
 </div>
 
 
 
-<p>{{ $categories[0]->news[0]->title }}</p>
+<div>
+    @foreach ($news as $new)
+        <div class="flex flex-col">
+            <p>{{ $new->title }}</p>
+
+        </div>
+    @endforeach
+</div>
