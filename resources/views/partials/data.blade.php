29b8b1eaea8097d47791b8326bddb772c1309dda
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-23 14:37:33 +0700

    feat: create new layout

--- resources/views/partials/data.blade.php
+++ resources/views/partials/data.blade.php
@@ -1,23 +1,21 @@
 {{-- @dd($categories->news) --}}
 
 
-<div class="wrapper mt-20">
-    <div class="heading-class flex  justify-between">
+<div class="wrapper mt-20 border  border-redHeavy">
+    <div class="heading-class flex border border-grey text-center items-center justify-between">
         <p class="font-poppins duration-300  hover:text-redHeavy  text-blackPrimary font-semibold text-2xl">Terbaru</p>
-        <div class="flex w-48 rounded-2xl gap-2 py-2 justify-center radius items-center text-center bg-radius box">
+        <div
+            class="flex w-48 durati/on-300 hover:opacity-80 rounded-2xl gap-2 py-2 justify-center radius items-center text-center bg-radius box">
             <p class='text-redHeavy text-lg font-medium font-poppins'>Selengkapnya</p>
             <img class="" width="12px" src="{{ asset('images/ArrowToRight.svg') }}" alt="Informa story">
         </div>
     </div>
-</div>
+    <div class="flex flex-row  ">
+        @foreach ($news as $new)
+            <div class="flex flex-col w-80">
+                <p>{{ $new->title }}</p>
 
-
-
-<div>
-    @foreach ($news as $new)
-        <div class="flex flex-col">
-            <p>{{ $new->title }}</p>
-
-        </div>
-    @endforeach
+            </div>
+        @endforeach
+    </div>
 </div>
