ec75ca9e5fe98d0b053c6cadc1fceb4843f30f05
Ridho Ray <ridhorayputra033@gmail.com>
2023-08-28 23:27:53 +0700

    chore: adding deatil page

--- resources/views/home/news.blade.php
+++ resources/views/home/news.blade.php
@@ -12,7 +12,8 @@
         <p>
             Dibuat pada: {{ $news->created_at->isoFormat('dddd, D MMM YYYY HH:mm') }} WIB
         </p>
-
+        <img class="rounded-3xl	" width="656" height="399" src="{{ asset('storage/images/' . $news->thumbnail) }}"
+            alt="" title="" />
     </div>
     <div>
         {{-- terpopuler right --}}
