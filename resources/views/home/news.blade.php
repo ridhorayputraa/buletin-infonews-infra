b1b49d5118fed5226c84ac72559be8e00d112ac9
Ridho Ray Putra <ridhorayputra033@gmail.com>
2023-08-28 11:08:48 +0700

    created news detail

--- resources/views/home/news.blade.php
+++ resources/views/home/news.blade.php
@@ -1,1 +1,20 @@
-@dd($news)
+{{-- @dd($news) --}}
+<div class="flex flex-row">
+    {{-- Wrapper --}}
+    <div>
+        {{-- Main content left --}}
+        <p>
+            {{ $news->title }}
+        </p>
+        <p class="bg-redHeavy">
+            {{ $news->authors->name }}
+        </p>
+        <p>
+            Dibuat pada: {{ $news->created_at->isoFormat('dddd, D MMM YYYY HH:mm') }} WIB
+        </p>
+
+    </div>
+    <div>
+        {{-- terpopuler right --}}
+    </div>
+</div>
