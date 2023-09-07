9c1ec42bdc7727a4f87c697df06f37ca4e128542
Ridho Ray <ridhorayputra033@gmail.com>
2023-09-07 22:01:57 +0700

    chore: adding microtinteraction in Detail page in news

--- resources/views/home/news.blade.php
+++ resources/views/home/news.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.main')
 @section('container')
-<div class="flex flex-row">
-    {{-- Wrapper --}}
     <div class="flex flex-row">
-        <div class="w-8/12 mr-9 border border-redHeavy">
+        {{-- Wrapper --}}
+        <div class="flex flex-row">
+            <div class="w-8/12 mr-9 border border-redHeavy">
 
-            {{-- Main content left --}}
-        <p class="text-blackPrimary font-serif	font-bold text-5xl ">
-            {{ $news->title }}
-        </p>
-        <p class="text-redHeavy font-poppins font-normal text-xl">
-            {{ $news->authors->name }}
-        </p>
-     
-        <p>
-            Dibuat pada: {{ $news->created_at->locale('id')->diffForHumans() }} 
-        </p>
-   
-        <img class="rounded-3xl	 max-w-full border border-redHeavy" width="100%" src="{{ asset('storage/images/' . $news->thumbnail) }}"
-            alt="{{$news->title}}" title="{{$news->title}}" />
-<p>
-    {{$news->photo_description}}
-</p>
+                {{-- Main content left --}}
+                <p class="text-blackPrimary font-serif	font-bold text-5xl ">
+                    {{ $news->title }}
+                </p>
+                <p class="text-redHeavy font-poppins font-normal text-xl">
+                    {{ $news->authors->name }}
+                </p>
+
+                <p>
+                    Dibuat pada: {{ $news->created_at->locale('id')->diffForHumans() }}
+                </p>
+
+                <img class="rounded-3xl	 max-w-full border border-redHeavy" width="100%"
+                    src="{{ asset('storage/images/' . $news->thumbnail) }}" alt="{{ $news->title }}"
+                    title="{{ $news->title }}" />
+                <p>
+                    {{ $news->photo_description }}
+                </p>
+
+            </div>
+
+            <div class="w-4/12 border mt-5 ml-9 border-redHeavy">
+                {{-- terpopuler right --}}
+                <p class="Terpopuler text-blackPrimary font-normal text-2xl font-poppins">Terpopuler</p>
+            </div>
 
         </div>
-
-        <div class="w-4/12 border ml-9 border-redHeavy">
-            {{-- terpopuler right --}}
-            <p class="">Terpopuler</p>
-        </div>
-
-     </div>
-</div>
-
+    </div>
 @endsection
